0b1621e8701700c65f7a2c03c574731ad3f7cd2a
jomarberdejo <berdejojomar@gmail.com>
2023-10-09 14:55:25 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-tasks';
+  title: string = 'My Tasks';
+  tasks: string[]=  JSON.parse(localStorage.getItem('taskList')) || [];
+  newTask: string = '';
+  editIndex: number | null = null;
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      if (this.editIndex !== null) {
+        this.tasks[this.editIndex] = this.newTask;
+        this.editIndex = null; 
+        this.saveTask();
+      } else {
+        this.tasks.push(this.newTask);
+        this.saveTask();
+      }
+      this.newTask = ''; 
+    }
+  }
+
+  
+
+  editTask(index: number) {
+
+    this.newTask = this.tasks[index];
+    this.editIndex = index; 
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+    
+    this.saveTask();
+    if (this.editIndex === index) {
+      this.newTask = ''; 
+      this.editIndex = null;
+    }
+  }
+  
+  saveTask(){
+    localStorage.setItem('taskList', JSON.stringify(this.tasks));
+  }
 }
